76aa0f5863894350f095b707e4a327c9c715e921
Keev003 <keev003@student.kristiania.no>
2025-05-06 18:47:29 +0200

    working on publishing to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/kws2100-exam-Keev003",
+  base: "/kws2100-exam-Keev003/",
   plugins: [react()],
 });
