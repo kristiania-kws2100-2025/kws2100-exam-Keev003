f2aa5ee1165240dc29f5869440ff8f0ddc5b32ab
Keev003 <keev003@student.kristiania.no>
2025-05-07 06:42:43 +0200

    rettet opp skrivefeil

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/kws2100-exam-Keev003/",
+  base: "/kws2100-exam-Keev003",
   server: {
     proxy: {
       "/kws2100-exam-Keev003/api": {
