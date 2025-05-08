d630e6e889a52a3aef27afde0237f2e66b25fdce
Keev003 <142782017+Keev003@users.noreply.github.com>
2025-05-08 23:32:51 +0200

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useGeographic } from "ol/proj";
-import { createRoot } from "react-dom/client";
-import "ol/ol.css";
-import { Application } from "./modules/app/application";
-
-useGeographic();
-
-createRoot(document.getElementById("root")!).render(<Application />);
